27d10bc5e5f70f59374ab7f1b6296b1486d0a4bb
Serge Rodovnichenko <sergerod@gmail.com>
2023-04-05 23:39:36 +0300

    fix php docblocks

--- src/Request/OrdersRequest.php
+++ src/Request/OrdersRequest.php
@@ -11,8 +11,14 @@ namespace SergeR\Webasyst\FivepostSDK\Request;
 
 use SergeR\Webasyst\FivepostSDK\{Client, RequestInterface, Response\OrdersResponse};
 
+/**
+ *
+ */
 class OrdersRequest implements RequestInterface, \JsonSerializable
 {
+    /**
+     * @var array
+     */
     protected array $orders = [];
 
     /**
@@ -24,33 +30,59 @@ class OrdersRequest implements RequestInterface, \JsonSerializable
     }
 
 
+    /**
+     * @param array $order
+     * @return $this
+     */
     public function addOrder(array $order): static
     {
         $this->orders[] = $order;
         return $this;
     }
 
+    /**
+     * @param Client $client
+     * @return OrdersResponse
+     * @throws \SergeR\Webasyst\FivepostSDK\Exceptions\AuthenticationKeyError
+     * @throws \SergeR\Webasyst\FivepostSDK\Exceptions\FivepostSDKException
+     * @throws \SergeR\Webasyst\FivepostSDK\Exceptions\UnexpectedResponse
+     * @throws \waException
+     * @throws \waNetException
+     * @throws \waNetTimeoutException
+     */
     public function send(Client $client): OrdersResponse
     {
         $net = $client->query($this);
         return new OrdersResponse($net->getResponse());
     }
 
+    /**
+     * @return string
+     */
     public function getEndpoint(): string
     {
         return '/api/v3/orders';
     }
 
+    /**
+     * @return string
+     */
     public function getHttpMethod(): string
     {
         return 'POST';
     }
 
+    /**
+     * @return array[]
+     */
     public function getData(): array
     {
         return ['partnerOrders' => $this->orders];
     }
 
+    /**
+     * @return array[]
+     */
     public function jsonSerialize(): array
     {
         return $this->getData();
